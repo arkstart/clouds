ac90886f1f14fdb341cd7fbe2bb5fe20d5263745
stanley355 <winatastanley355@gmail.com>
2022-03-30 17:54:37 +0700

    add setup

--- src/schema.rs
+++ src/schema.rs
@@ -6,18 +6,3 @@ table! {
         url -> Varchar,
     }
 }
-
-table! {
-    shortenurl (id) {
-        id -> Int4,
-        origin_url -> Varchar,
-        hashed_url -> Nullable<Varchar>,
-        custom_url -> Nullable<Varchar>,
-        redirection_count -> Nullable<Int4>,
-    }
-}
-
-allow_tables_to_appear_in_same_query!(
-    host,
-    shortenurl,
-);
