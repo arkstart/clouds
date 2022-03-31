6f501d84b45f98e9d5dc4fefcb33c86c71019681
stanley355 <winatastanley355@gmail.com>
2022-03-31 09:03:44 +0700

    fix migration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use dotenv::dotenv;
+use std::env;
+
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello!")
+}
+
+async fn serve_web(address: String) -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello World!" }))
+            .service(greet)
+    })
+    .bind(address)?
+    .run()
+    .await
+} 
+
+#[actix_web::main] 
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host = &env::var("HOST").unwrap_or("127.0.0.1".to_string());
+    let port = &env::var("PORT").unwrap_or("8080".to_string());
+    let address = format!("{}:{}", host, port);
+
+    serve_web(address).await
 }
