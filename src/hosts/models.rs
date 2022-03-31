9cb54f12152798c4f49542bf57a77d9928dc4c87
stanley355 <winatastanley355@gmail.com>
2022-03-31 20:46:32 +0700

    fix bug

--- src/hosts/models.rs
+++ src/hosts/models.rs
@@ -1,5 +1,5 @@
 use actix_web::web;
-use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl};
+use diesel::{ExpressionMethods, QueryDsl, RunQueryDsl, sql_query};
 use serde::{Deserialize, Serialize};
 
 use crate::db::PgPool;
@@ -10,14 +10,4 @@ pub struct Hosts {
   pub name: String,
   pub description: String,
   pub url: String,
-}
-
-impl Hosts {
-  pub fn check_existing(host_name: String, pool: web::Data<PgPool>) -> Option<bool> {
-    use crate::schema::hosts::dsl::*;
-    let conn = &pool.get().unwrap();
-    let existing_host = hosts.select(name.eq(host_name)).execute(conn).unwrap();
-
-    return Some(existing_host != 0)
-  }
 }
