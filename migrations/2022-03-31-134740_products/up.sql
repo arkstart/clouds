cfb2c31ff79eb5444e73616ff2ee75597ddc85c9
stanley355 <winatastanley355@gmail.com>
2022-04-01 09:29:36 +0700

    fix migration

--- migrations/2022-03-31-134740_products/up.sql
+++ migrations/2022-03-31-134740_products/up.sql
@@ -1,14 +1,11 @@
 -- Your SQL goes here
 CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
+    hosts_id integer NOT NULL,
     name VARCHAR NOT NULL,
     description VARCHAR NOT NULL,
     url VARCHAR NOT NULL,
     free BOOLEAN,
     pricing VARCHAR,
-    
-    build_limit VARCHAR,
-    bandwith_limit VARCHAR
-    site_limit VARCHAR
     FOREIGN KEY (hosts_id) REFERENCES hosts (id)
 )
