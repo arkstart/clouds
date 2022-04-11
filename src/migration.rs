1b34b3ee65b5aa19edb6536a049af5b3acc731a0
stanley355 <winatastanley355@gmail.com>
2022-04-11 13:25:28 +0700

    hotfix migration

--- src/migration.rs
+++ src/migration.rs
@@ -1,24 +1,10 @@
-use crate::lib::error::*;
-use actix_web::{http::header, HttpRequest, HttpResponse};
-use std::env;
+use actix_web::HttpResponse;
 use std::process::Command;
 
-pub async fn migrate(req: HttpRequest) -> HttpResponse {
-  let req_auth = req.headers().get(header::AUTHORIZATION);
-  let auth_token = env::var("AUTHORIZATION_TOKEN").expect("AUTHORIZATION_TOKEN must be set");
-
-  match req_auth {
-    Some(token) => {
-      if token.to_str().unwrap() == auth_token {
-        let cmd = Command::new("diesel")
-          .args(["migration", "run"])
-          .spawn()
-          .expect("failed to execute process");
-        HttpResponse::Ok().body(format!("{:?}", cmd.stdout))
-      } else {
-        ErrResponse::new_message(ErrType::Unauthorized, "Invalid Authorization".to_string())
-      }
-    }
-    None => ErrResponse::new_message(ErrType::Unauthorized, "Authorization not set".to_string()),
-  }
+pub async fn migrate() -> HttpResponse {
+  let cmd = Command::new("diesel")
+    .args(["migration", "run"])
+    .spawn()
+    .expect("failed to execute process");
+  HttpResponse::Ok().body(format!("{:?}", cmd.stdout))
 }
