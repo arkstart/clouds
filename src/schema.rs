b35361a38bb1b57122ee1932502edf11327a4f5d
stanley355 <winatastanley355@gmail.com>
2022-04-22 10:23:26 +0700

    plan migration

--- src/schema.rs
+++ src/schema.rs
@@ -15,6 +15,36 @@ table! {
 }
 
 table! {
+    plans (id) {
+        id -> Int4,
+        hosts_id -> Int4,
+        name -> Nullable<Varchar>,
+        description -> Nullable<Varchar>,
+        price -> Nullable<Int4>,
+        price_unit -> Nullable<Varchar>,
+        price_timeunit -> Nullable<Varchar>,
+        price_desc -> Nullable<Varchar>,
+        concurrent_build -> Nullable<Int4>,
+        concurrent_build_unit -> Nullable<Varchar>,
+        concurrent_build_timeunit -> Nullable<Varchar>,
+        concurrent_build_desc -> Nullable<Varchar>,
+        bandwidth -> Nullable<Int4>,
+        bandwidth_unit -> Nullable<Varchar>,
+        bandwidth_timeunit -> Nullable<Varchar>,
+        bandwidth_desc -> Nullable<Varchar>,
+        build -> Nullable<Int4>,
+        build_unit -> Nullable<Varchar>,
+        build_timeunit -> Nullable<Varchar>,
+        build_desc -> Nullable<Varchar>,
+        analytic -> Nullable<Bool>,
+        analytic_price -> Nullable<Int4>,
+        analytic_unit -> Nullable<Varchar>,
+        analytic_timeunit -> Nullable<Varchar>,
+        analytic_desc -> Nullable<Varchar>,
+    }
+}
+
+table! {
     products (id) {
         id -> Int4,
         hosts_id -> Int4,
@@ -31,9 +61,11 @@ table! {
     }
 }
 
+joinable!(plans -> hosts (hosts_id));
 joinable!(products -> hosts (hosts_id));
 
 allow_tables_to_appear_in_same_query!(
     hosts,
+    plans,
     products,
 );
